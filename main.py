eabd616bc8d039c9775bebd351818a8f5120bfe7
dbdedw <70300014+dbdedw@users.noreply.github.com>
2020-09-23 15:53:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
-import requests
+import  os, time, ctypes, datetime, os.path, tkinter
 from requests_html import HTMLSession
-import requests, os, threading, sys, time, random, ctypes, webbrowser,re, hashlib, datetime, os.path, tkinter
-from colorama import Fore, init
-from re import search
-import datetime
-from tkinter import filedialog, messagebox
+from tkinter import filedialog
 e = datetime.datetime.now()
-
-current_date = e.strftime("%Y-%m-%d-%H-%M-%S")
-session = HTMLSession()
 root = tkinter.Tk()
 root.withdraw()
 
+session = HTMLSession()
+
+if not os.path.exists("./input.txt"):
+    f= open("input.txt","w+")
 if not os.path.exists("results"):
     os.makedirs("results/")
-if not os.path.exists('results/untaken'):
-    os.makedirs('results/untaken')
-if not os.path.exists('results/taken'):
-    os.makedirs('results/taken')
-if not os.path.exists('results/untaken/{}'.format(current_date)):
-    os.makedirs('results/untaken/{}'.format(current_date))
-if not os.path.exists('results/taken/{}'.format(current_date)):
-    os.makedirs('results/taken/{}'.format(current_date))
+if not os.path.exists("results/untaken"):
+    os.makedirs("results/untaken")
+if not os.path.exists("results/taken"):
+    os.makedirs("results/taken")
 
-username = input("Fortnite Name: ")
+def load_combos():
+    global emails
+    print()
+    input("  Press ENTER to select combos..")
+    fileNameCombo = filedialog.askopenfile(parent=root, mode='rb', title='Choose a combo file',
+                                filetype=(("txt", "*.txt"), ("All files", "*.txt")))
+    if fileNameCombo is None:
+        print()
+        print("  Please select valid combo file..")
+        time.sleep(2)
+    else:
+        try:
+            with open(fileNameCombo.name, 'r+', encoding='utf-8') as e:
+                ext = e.readlines()
+                for line in ext:
+                    try:
+                        email = line.split()[0].replace('\n', '')
+                        emails.append(email)
+                    except:
+                        pass
+            print("  Loaded [{}] combos lines..".format(len(email)))
+            time.sleep(2)
+        except Exception:
+            print("  Your combo file is probably harmed, please try again..")
+            time.sleep(2)
+
+username = emails
 session = HTMLSession()
 
 r = session.get(f"https://fortnitetracker.com/profile/search?q={username}")
 if r.html.search('404 Not Found'):
   print(f"Name: '{username}' is available!")
-  open('results/untaken/{}/untaken.txt'.format(current_date), 'a+').write("{}".format(username))
+  open('results/untaken/untaken.txt', 'a+').write("{}\n".format(username))
 
 if r.html.search('Overview'):
-  open('results/taken/{}/taken.txt'.format(current_date), 'a+').write("{}".format(username))
+  open('results/taken/taken.txt', 'a+').write("{}\n".format(username))
